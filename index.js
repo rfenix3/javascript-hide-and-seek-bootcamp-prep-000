720475105b8d5bff7725a115ff74e027c416a30c
Richard Fenix <richard.fenix@gmail.com>
2018-03-01 23:22:27 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+   return document.getElementById('nested').querySelector('div.target');
+} 
+
+function increaseRankBy(n){
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i=0; i<lis.length; i++){
+    var parsed = parseInt(lis[i].innerHTML); 
+    lis[i].innerHTML = (parsed + n);
+    console.log(lis[i].innerHTML);
+  }
+}
+//increaseRankBy(1);
+
+function deepestChild(){
+	//return document.querySelectorAll('div#grand-node');
+	return document.querySelector('#grand-node div div div div');
+}
