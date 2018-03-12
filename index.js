2b9589d2dfcf7cae58b4ae1377b7f4b46b79c7de
Richard Fenix <richard.fenix@gmail.com>
2018-03-12 13:39:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,16 @@ function increaseRankBy(n){
 //increaseRankBy(1);
 
 function deepestChild(){
-	//return document.querySelectorAll('div#grand-node');
-	//document.getelementById(id).querySelectorAll()
-	return document.querySelector('#grand-node div div div div');
+ let node = document.getElementById('grand-node');
+  let nextNode = node.children[0];
+
+  while (nextNode) {
+    node = nextNode;
+    nextNode = node.children[0];
+  }
+
+  return node;
+	// node = '#grand-node div div div div';
+	
 }
+
